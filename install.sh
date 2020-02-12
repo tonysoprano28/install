1ad2194ec17a8d93342992b241ac5cb46b38dbf1
tonysoprano28 <50218264+tonysoprano28@users.noreply.github.com>
2020-02-12 03:30:47 +0000

    morpheus install kali 202

---
+++ install.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/r00t-3xp10it/morpheus
+sudo apt-get install zenity
+   29  cd
+   30  cd morpheus/
+   31  sudo apt-get update
+   32  sudo ./morpheus.sh
+   33  sudo apt-get install  sslstrip
+   34  sudo apt-get install  dnspython
+   35  sudo apt-get install  dnspython2
+   36  sudo pip install dnspython
+   37  sudo apt-get install pip
+   38  pip install dnspython
+   39  sudo pip install dnspython
+   40  apt install python-pip
+   41  sudo apt install python-pip
+   42  sudo pip install dnspython
+   43  sudo ./morpheus.sh
+   44  clear
+   45  exit
+   46  cd morpheus/
+   47  sudo ./morpheus.sh 
+
